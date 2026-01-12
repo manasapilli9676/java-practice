3afea9cd06240a1a896c661fec8e37f0cf49eafa
Manasa <pillymanasa@gmail.com>
2026-01-12 15:18:05 +0530

    Daily progress

--- src/Manasa/com/Manasa.java
+++ src/Manasa/com/Manasa.java
@@ -2,6 +2,6 @@ package Manasa.com;
 
 public class Manasa {
     public static void main(String[] args) {
-        System.out.println("i am manasa");
+        System.out.println("i am manasa");// hi iam manasa
     }
 }
