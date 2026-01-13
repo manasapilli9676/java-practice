f91ae59e6d3335b5805af4767c57700ad5340e40
Manasa <pillymanasa@gmail.com>
2026-01-13 20:35:04 +0530

    Daily progress

--- src/Manasa/com/ReverseAStringIn.java
+++ src/Manasa/com/ReverseAStringIn.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ReverseAStringIn {
     public static void main(String[] args) {
        Scanner input=new Scanner(System.in);
-        System.out.println("hmm");
+        System.out.println("hmm");//hi
       {
        }
 
