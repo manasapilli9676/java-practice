8a8b96fed1eea4197ce5a03c0327851b46081a38
Manasa <pillymanasa@gmail.com>
2026-01-14 22:34:46 +0530

    Daily progress

---
+++ src/Manasa/com/ReverseAString.java
@@ -0,0 +1,10 @@
+package Manasa.com;
+import java.util.Scanner;
+public class ReverseAString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the string to print reverse");
+        char name = input.next().charAt(0);
+        int reverse= ' ';
+    }
+}
