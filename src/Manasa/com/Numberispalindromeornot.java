3a4c509757dde506237b658c51db17460aa6215a
Manasa <pillymanasa@gmail.com>
2026-01-17 12:32:08 +0530

    Daily progress

--- src/Manasa/com/Numberispalindromeornot.java
+++ src/Manasa/com/Numberispalindromeornot.java
@@ -4,6 +4,19 @@ public class Numberispalindromeornot {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("enter the number is palindrome or not");
+        int num=input.nextInt();
+        int palindrome=0;
+        while(num>0){
+            int rem=num%10;
+            palindrome=num*10+rem;
+                    num=num/10;
+        }
+        if(palindrome==num){
+            System.out.println("given number is palindrome");
+        }
+        else{
+            System.out.println("given number not is palindrome");
+        }
 
     }
 }
