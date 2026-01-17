01be67038cbfc677937da3ef0aea1227fc458945
Manasa <pillymanasa@gmail.com>
2026-01-17 13:14:53 +0530

    Daily progress

---
+++ src/Manasa/com/HCFOfTwoNumbersProgram.java
@@ -0,0 +1,8 @@
+package Manasa.com;
+import java.util.Scanner;
+public class HCFOfTwoNumbersProgram {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+    }
+}
