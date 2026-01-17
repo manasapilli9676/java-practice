8e02c61f1e15946c37a99162eb2aad6cf8b1b355
Manasa <pillymanasa@gmail.com>
2026-01-17 17:35:27 +0530

    Daily progress

---
+++ src/Manasa/com/PerfectNumber/Java.java
@@ -0,0 +1,24 @@
+package Manasa.com.PerfectNumber;
+import java.util.Scanner;
+public class Java {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        int i=1;
+        int next=0;
+        while(i<=num){
+            if(num%i==0){
+                System.out.println(i);
+            }
+            next=next+i;
+        }
+            if(next==num){
+                System.out.println("the given number is perfect number");
+            }
+            else {
+                System.out.println("given number ics not a perfect number");
+            }
+        }
+
+    }
+
