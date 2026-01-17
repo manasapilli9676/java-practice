f6daaaebc34901950b1c28bb1be349ad4326d0db
Manasa <pillymanasa@gmail.com>
2026-01-17 12:23:43 +0530

    Daily progress

---
+++ src/Manasa/com/Numberispalindromeornot.java
@@ -0,0 +1,9 @@
+package Manasa.com;
+import java.util.Scanner;
+public class Numberispalindromeornot {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the number is palindrome or not");
+
+    }
+}
