2c90c281685bffdd3f15e678f4f32565eb59b7c9
Manasa <pillymanasa@gmail.com>
2026-01-17 22:02:42 +0530

    Daily progress

---
+++ src/Manasa/com/Even.java
@@ -0,0 +1,18 @@
+package Manasa.com;
+import java.util.Scanner;
+public class Even {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the number of days in a month ");
+        int days=input.nextInt();
+        int result=0;
+
+                result=days/2;
+
+
+
+
+        System.out.println("leave days are "+result);
+    }
+}
+//
