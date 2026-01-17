81e4e8d7860880088fac450f047263e90717ec3b
Manasa <pillymanasa@gmail.com>
2026-01-17 15:48:02 +0530

    Daily progress

---
+++ src/Manasa/com/JavaProgramVowelOrConsonant.java
@@ -0,0 +1,11 @@
+package Manasa.com;
+import java.util.Scanner;
+public class JavaProgramVowelOrConsonant {
+    Scanner input=new Scanner(System.in);
+    char x=input.next().charAt(0);
+    String in="'a','e','i','o','u'";
+    if(x==in){
+        System.out.println("given input is consonant ");
+    }
+
+}
