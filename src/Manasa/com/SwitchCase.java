23aecac70fd2ef371b0fb19ad88175955f01f794
Manasa <pillymanasa@gmail.com>
2026-01-18 09:44:45 +0530

    Daily progress

---
+++ src/Manasa/com/SwitchCase.java
@@ -0,0 +1,25 @@
+package Manasa.com;
+import java.util.Scanner;
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String fruit=input.next();
+        switch(fruit){
+            case "mango":
+                System.out.println("mango is summer seasonal fruit ");
+                break;
+            case "guava":
+                System.out.println("guava is my favourite fruit ");
+                break;
+            case "watermelon":
+                System.out.println("watermelon consist of many seeds ");
+                break;
+            case "grapes":
+                System.out.println("i hate grapes ");
+                break;
+            default:
+                System.out.println("please enter the valid fruit name ");
+
+        }
+    }
+}
