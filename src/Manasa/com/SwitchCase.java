04065d2fc5785ce2e1db0bc647f90014aa232ceb
Manasa <pillymanasa@gmail.com>
2026-01-18 09:55:33 +0530

    Daily progress

--- src/Manasa/com/SwitchCase.java
+++ src/Manasa/com/SwitchCase.java
@@ -5,20 +5,25 @@ public class SwitchCase {
         Scanner input=new Scanner(System.in);
         String fruit=input.next();
         switch(fruit){
-            case "mango":
-                System.out.println("mango is summer seasonal fruit ");
-              break;
-            case "guava":
-                System.out.println("guava is my favourite fruit ");
-                break;
-            case "watermelon":
-                System.out.println("watermelon consist of many seeds ");
-                break;
-            case "grapes":
-                System.out.println("i hate grapes ");
-                break;
-            default:
-                System.out.println("please enter the valid fruit name ");
+//            case "mango":
+//                System.out.println("mango is summer seasonal fruit ");
+//              break;
+//            case "guava":
+//                System.out.println("guava is my favourite fruit ");
+//                break;
+//            case "watermelon":
+//                System.out.println("watermelon consist of many seeds ");
+//                break;
+//            case "grapes":
+//                System.out.println("i hate grapes ");
+//                break;
+//            default:
+//                System.out.println("please enter the valid fruit name ");
+            case "mango"-> System.out.println("mango is summer seasonal fruit ");
+            case "guava"->System.out.println("guava is my favourite fruit ");
+            case "watermelon"->System.out.println("watermelon consist of many seeds ");
+            case "grapes"->System.out.println("i hate grapes ");
+            default->System.out.println("please enter the valid fruit name ");
 
         }
     }
