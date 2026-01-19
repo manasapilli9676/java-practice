ed3e267ff1906b3122de2176d7ae3aa317a9d289
Manasa <pillymanasa@gmail.com>
2026-01-19 17:55:18 +0530

    Daily progress

--- src/Manasa/com/FunctionsOrmethods.java
+++ src/Manasa/com/FunctionsOrmethods.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 public class FunctionsOrmethods {
     public static void main(String[] args) {
-
+sum();
     }
     static void sum(){
         Scanner input=new Scanner(System.in);
