a9954c177acd06bce7088e4b5fcff98c22e40612
Manasa <pillymanasa@gmail.com>
2026-01-19 18:16:24 +0530

    Daily progress

--- src/Manasa/com/FunctionsOrmethods.java
+++ src/Manasa/com/FunctionsOrmethods.java
@@ -1,22 +1,41 @@
 //print sum of two numbers
-
+//return type basically call the return type block
  package Manasa.com;
 import java.util.Scanner;
 public class FunctionsOrmethods {
     public static void main(String[] args) {
-sum();
-sum();
+        sum();
+//sum();
+//sum();
+//    }
+//    static void sum(){
+//        Scanner input=new Scanner(System.in);
+//        int num1=input.nextInt();
+//        int num2=input.nextInt();
+//        int sum=num1+num2;
+//        System.out.println("the sum is "+sum);
+//    }
+//    /*return_type name(){  return
+//     //body
+//     return statement;
+//     }
+//    */
+
     }
-    static void sum(){
-        Scanner input=new Scanner(System.in);
-        int num1=input.nextInt();
-        int num2=input.nextInt();
-        int sum=num1+num2;
-        System.out.println("the sum is "+sum);
+    static int sum2(){
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        return sum2();
     }
-    /*return_type name(){  return
-     //body
-     return statement;
-     }
-    */
+
+    static void sum() {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("the sum is " + sum);
+    }
 }
