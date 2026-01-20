8447cba46d82f6dc2d6a7dbaf3077d4984326470
Manasa <pillymanasa@gmail.com>
2026-01-20 22:03:54 +0530

    Daily progress

---
+++ src/Manasa/com/Swap.java
@@ -0,0 +1,11 @@
+package Manasa.com;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+}
