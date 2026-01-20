63b89635c745498cdad159371884e2e94c49c129
Manasa <pillymanasa@gmail.com>
2026-01-20 21:19:41 +0530

    Daily progress

--- src/Manasa/com/ReturnStringusingmethods.java
+++ src/Manasa/com/ReturnStringusingmethods.java
@@ -2,7 +2,8 @@ package Manasa.com;
 import java.util.Scanner;
 public class ReturnStringusingmethods {
     public static void main(String[] args) {
-String greetings =name();
+String greet =name();
+        System.out.println(greet);
     }
     static String name(){
         String greeting="happy birthday";
