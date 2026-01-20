46dd343c7eb543b57c17588c865d3aeecc798a2a
Manasa <pillymanasa@gmail.com>
2026-01-20 22:23:47 +0530

    Daily progress

--- src/Manasa/com/Swap.java
+++ src/Manasa/com/Swap.java
@@ -4,10 +4,18 @@ public class Swap {
     public static void main(String[] args) {
         int a=23;
         int b=43;
+        swap(2,3);
+//        int temp=a;
+//        a=b;
+//        b=temp;
+        //by using the functions
+        System.out.println(a +" "+b);
+
+    }
+    static void swap(int a,int b){
         int temp=a;
         a=b;
         b=temp;
-        System.out.println(a +" "+b);
 
     }
 }
