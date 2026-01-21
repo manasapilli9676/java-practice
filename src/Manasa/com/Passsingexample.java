5055158bfb86299ad591d958357edbfd1c11d855
Manasa <pillymanasa@gmail.com>
2026-01-21 14:24:35 +0530

    Daily progress

---
+++ src/Manasa/com/Passsingexample.java
@@ -0,0 +1,11 @@
+package Manasa.com;
+
+public class Passsingexample {
+    public static void main(String[] args) {
+        String name="manasa";
+        greet(name);
+    }
+    static  void greet(String naam){
+        System.out.println(naam);
+    }
+}
