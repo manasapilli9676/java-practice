a2b38387587948dec25899a91e46a3873fbb3065
Manasa <pillymanasa@gmail.com>
2026-01-21 18:29:13 +0530

    Daily progress

---
+++ src/Manasa/com/Scope.java
@@ -0,0 +1,36 @@
+package Manasa.com;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println(a);
+        random(2);
+
+        {
+            //block scope
+            //in this block scope if we initialize hte variables number of times it show the error
+             // we can change the value but we cannot initialize
+            // int a=23;we cannot initialized like this
+            //
+             a=54;
+             int c=99;
+            System.out.println(a);
+
+        }
+        System.out.println(a);
+        //System.out.println(c);we cant print c out side the block
+
+    }
+    //it is the function scope
+    //it does allow any other variables to access in it if
+    //if we use it it may show error
+    // here we can initialize number of times see with in a same program i hava initialized a and b two times ther is no change
+    static void random(int marks){
+        int a=2;
+        int b=3;
+       int num=a+b;
+        System.out.println(num);
+        System.out.println(a);
+    }
+}
