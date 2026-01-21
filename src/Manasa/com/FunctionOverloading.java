a1b5b53f76f49163b5ebda75853f52a674458754
Manasa <pillymanasa@gmail.com>
2026-01-21 22:12:58 +0530

    Daily progress

--- src/Manasa/com/FunctionOverloading.java
+++ src/Manasa/com/FunctionOverloading.java
@@ -4,7 +4,12 @@ public class FunctionOverloading {
     public static void main(String[] args) {
         num1("manasa");
         num1(4);
+        int sum=num1(2,4);
 
+    }
+    static int num1(int a,int b){
+        int sum1=a+b;
+        return sum1;
     }
     static void num1(int a){
         System.out.println("first one");
