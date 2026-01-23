081686e1b023fb4284da2a1d6b7462354a900b6b
Manasa <pillymanasa@gmail.com>
2026-01-23 18:29:32 +0530

    Daily progress

--- src/Manasa/com/Sum.java
+++ src/Manasa/com/Sum.java
@@ -2,7 +2,17 @@ package Manasa.com;
 import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        int ans=add(num1,num2);
+        System.out.println("sum of two numbers"+ans);
 
     }
+    static int add(int num1,int num2){
+        int sum=num1+num2;
+        return sum;
+    }
+
 
 }
