79e3aab86ea91b7a39a7b616dfb7eed25b9b6cb8
Manasa <pillymanasa@gmail.com>
2026-01-23 10:26:07 +0530

    Daily progress

---
+++ src/Manasa/com/CheckprimeInMethods.java
@@ -0,0 +1,30 @@
+package Manasa.com;
+import java.util.Scanner;
+public class CheckprimeInMethods {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int n= input.nextInt();
+        boolean ans=isprime(n);
+        System.out.println();
+    }
+
+    static boolean isprime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        if(c*c>n){
+            return true;
+        }
+        return false;
+    }
+}
