1ba7bf663fc2c4e9355bbd73983a8a443561aa2b
Manasa <pillymanasa@gmail.com>
2026-01-25 22:16:52 +0530

    Daily progress

---
+++ src/Manasa/com/PrimeOrNotFunction.java
@@ -0,0 +1,15 @@
+package Manasa.com;
+import java.util.Scanner;
+public class PrimeOrNotFunction {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        boolean prime=
+
+    }
+    static  boolean prime(int num){
+        if(num<=1){
+            return false;
+        }
+
+}
