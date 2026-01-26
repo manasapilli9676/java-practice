780b5d66135af5330e9f22be40076d44b89d70bd
Manasa <pillymanasa@gmail.com>
2026-01-26 11:16:13 +0530

    Daily progress

---
+++ src/Manasa/com/PalindromeMethodjava.java
@@ -0,0 +1,9 @@
+package Manasa.com;
+import java.util.Scanner;
+public class PalindromeMethodjava {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+    }
+
+}
