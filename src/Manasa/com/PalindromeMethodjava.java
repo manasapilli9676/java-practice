44f33444d38a3a4660514ba74ba1e0d08858c67c
Manasa <pillymanasa@gmail.com>
2026-01-27 18:36:38 +0530

    Daily progress

--- src/Manasa/com/PalindromeMethodjava.java
+++ src/Manasa/com/PalindromeMethodjava.java
@@ -3,6 +3,24 @@ import java.util.Scanner;
 public class PalindromeMethodjava {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        boolean palin=palindrome(num);
+       if(palin){
+           System.out.println("it is a palindrome");
+       }
+       else{
+           System.out.println("it is not a palindrom");
+       }
+
     }
-    static void palindrome()
+    static boolean palindrome(int num){
+        int x=num;
+        int reverse=0;
+        while(x>0){
+            int rem=x%10;
+            reverse=reverse*10+rem;
+            x=x/10;
+        }
+        return reverse==num;
+    }
 }
