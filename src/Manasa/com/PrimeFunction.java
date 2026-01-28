bcfdfe90fe4b9a997848e143e15426451354bf35
Manasa <pillymanasa@gmail.com>
2026-01-28 09:46:56 +0530

    Daily progress

--- src/Manasa/com/PrimeFunction.java
+++ src/Manasa/com/PrimeFunction.java
@@ -1,19 +1,36 @@
 package Manasa.com;
 import java.util.Scanner;
+
 public class PrimeFunction {
+
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int num1=input.nextInt();
-        int num2=input.nextInt();
 
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
 
+        prime(num1, num2);   // function call
     }
-    static void prime(int num1,int num2)
-        int num1;
-    int num2;
-    for(int i=num1;i<=num2;i++){
-        int result=num1/num1;
 
+    static void prime(int num1, int num2) {
+
+        for (int i = num1; i <= num2; i++) {
+
+            if (i <= 1)
+                continue;
+
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
     }
-e
 }
