ddd2fc076ccd9c3afc0dad24988ba2dfcfaea19d
Manasa <pillymanasa@gmail.com>
2026-01-31 20:52:18 +0530

    Daily progress

--- src/Manasa/com/ArrayOfobj.java
+++ src/Manasa/com/ArrayOfobj.java
@@ -6,6 +6,7 @@ public class ArrayOfobj {
         Scanner input=new Scanner(System.in);
 //        int[] arr=new int[5];
 //        for(int i=0;i<arr.length;i++){
+
 //            arr[i]=input.nextInt();
 
 //
