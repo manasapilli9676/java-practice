56e75c1db8d14907f1a8b6861c10d94a0bd59d06
Manasa <pillymanasa@gmail.com>
2026-02-01 11:23:10 +0530

    Daily progress

--- src/Manasa/com/ArrayFunctions.java
+++ src/Manasa/com/ArrayFunctions.java
@@ -4,16 +4,20 @@ import java.util.Arrays;
 public class ArrayFunctions {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        change(arr);
+        int[] nums={2,3,4,5,6,5};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
-    static void change(int[] arr){
-        arr[0]=3;
-        arr[1]=32;
-        arr[2]=44;
-        arr[3]=56;
-        arr[4]=435;
-
-        System.out.println(arr[2]);
+    static void change(int[] nums){
+//        arr[0]=3;
+//        arr[1]=32;
+//        arr[2]=44;
+//        arr[3]=56;
+//        arr[4]=435;
+//
+//        System.out.println(arr[2]);
+        nums[3]=20;
     }
 }
