99603d2f98bdbeafcd27cc7d9e04c4b9479bdb15
Manasa <pillymanasa@gmail.com>
2026-02-01 12:20:25 +0530

    Daily progress

--- src/Manasa/com/TwodArrays.java
+++ src/Manasa/com/TwodArrays.java
@@ -1,4 +1,23 @@
 package Manasa.com;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class TwodArrays {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int[][] num=new int[3][2];
+        System.out.println(num.length);
+        for(int row=0;row<num.length;row++){
+            for(int col=0;col<num[row].length;col++);{
+                num[row][col]=input.nextInt();
+            }
+
+        }
+//        int[][] arr={
+//                {1,2,3},
+//                {4,5,6},
+//                {65,6,57},
+//        };
+        System.out.println(Arrays.toString(num));
+
+    }
 }
