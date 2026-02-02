d224e458161829df06ea2046818fc89574125eab
Manasa <pillymanasa@gmail.com>
2026-02-02 17:58:23 +0530

    Daily progress

---
+++ src/Manasa/com/MaxInArray.java
@@ -0,0 +1,11 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class MaxInArray {
+    public static void main(String[] args) {
+        int[] arr1={1,2,3,5,4,8};
+
+    }
+    static void max(int[] arr1,int max)
+
+}
