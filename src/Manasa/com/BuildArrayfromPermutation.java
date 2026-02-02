b2d984e37aeacd36c2bbc1bc72a8577a716d6bcd
Manasa <pillymanasa@gmail.com>
2026-02-02 21:41:53 +0530

    Daily progress

--- src/Manasa/com/BuildArrayfromPermutation.java
+++ src/Manasa/com/BuildArrayfromPermutation.java
@@ -1,4 +1,8 @@
 package Manasa.com;
-
+import java.util.Scanner
 public class BuildArrayfromPermutation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+
 }
