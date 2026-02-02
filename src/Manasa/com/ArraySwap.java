0f07001032d0c984b00f2532f61327e1cc69fe7e
Manasa <pillymanasa@gmail.com>
2026-02-02 17:47:15 +0530

    Daily progress

--- src/Manasa/com/ArraySwap.java
+++ src/Manasa/com/ArraySwap.java
@@ -6,15 +6,15 @@ public class ArraySwap {
     public static void main(String[] args) {
         int[] arr1={1,2,3,4,56,6};
 //        int[] arr2={23,4,45,6,7,78};
-        swap(3,4,arr1);
+        swap(arr1,3,4);
 
         System.out.println(Arrays.toString(arr1));
 
     }
-    static void swap(int index1,int index2,int[] arr1){
-       int temp=index1;
-       index2=index1;
-       temp=index2;
+    static void swap(int[] arr1,int index1,int index2){
+       int temp=arr1[index1];
+       arr1[index2]=arr1[index1];
+       temp=arr1[index2];
 
     }
 }
