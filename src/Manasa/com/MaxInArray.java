292b21ab099c03c9f9abd438f857fff8eeacf4c3
Manasa <pillymanasa@gmail.com>
2026-02-02 18:26:07 +0530

    Daily progress

--- src/Manasa/com/MaxInArray.java
+++ src/Manasa/com/MaxInArray.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class MaxInArray {
     public static void main(String[] args) {
-        int[] arr1={1,2,3,5,4,8};
+        int[] arr={1,2,3,5,4,8};
+        System.out.println(max(arr));
 
     }
-    static void max(int[] arr1,int max){
-        for(int i=0;i<Arrays.length();)
+    static int max(int[] arr){
+        int maxval=arr[0];
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]>maxval){
+                maxval=arr[i];
+               }
+
+
+
+        }
+        return maxval;
+
 
     }
 
