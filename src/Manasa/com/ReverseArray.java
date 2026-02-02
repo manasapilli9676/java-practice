923ae34be4867bf2bc314dabdf94f47d32b052e0
Manasa <pillymanasa@gmail.com>
2026-02-02 18:58:24 +0530

    Daily progress

--- src/Manasa/com/ReverseArray.java
+++ src/Manasa/com/ReverseArray.java
@@ -1,4 +1,9 @@
 package Manasa.com;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+    }
 }
