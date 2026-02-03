f81f178695a7746fe658183311328fd525a43a03
Manasa <pillymanasa@gmail.com>
2026-02-03 21:18:12 +0530

    Daily progress

--- src/Manasa/com/ConcatenationofArray.java
+++ src/Manasa/com/ConcatenationofArray.java
@@ -4,7 +4,22 @@ import java.util.Arrays;
 public class ConcatenationofArray {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int i=input.nextInt();
-        int[] arr=new
+        int x=input.nextInt();
+        int[] arr=new int[x];
+
+        int[] ans=new int[2*x];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.nextInt();
+        }
+        for(int i=0;i<x;i++){
+            ans[i]=arr[i];
+        }
+        for(int i=0;i<x;i++){
+            ans[i+x]=arr[i];
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(ans));
+
     }
 }
