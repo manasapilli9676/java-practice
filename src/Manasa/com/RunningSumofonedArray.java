bbc5ccf9e8456031d185b0b694209198314a3990
Manasa <pillymanasa@gmail.com>
2026-02-04 21:36:48 +0530

    Daily progress

--- src/Manasa/com/RunningSumofonedArray.java
+++ src/Manasa/com/RunningSumofonedArray.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class RunningSumofonedArray {
+
     public static void main(String[] args) {
 
         Scanner input=new Scanner(System.in);
