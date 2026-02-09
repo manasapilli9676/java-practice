7ba106f0b2d959da42893f11a7e761d09c8cca63
Manasa <pillymanasa@gmail.com>
2026-02-09 19:17:00 +0530

    Daily progress

--- src/Manasa/com/RunningSumofonedArray.java
+++ src/Manasa/com/RunningSumofonedArray.java
@@ -1,15 +1,22 @@
 package Manasa.com;
 import java.util.Scanner;
-
 import java.util.Arrays;
-
 public class RunningSumofonedArray {
     public static void main(String[] args) {
-
         Scanner input=new Scanner(System.in);
 
         int x=input.nextInt();
-
-
+        int[] arr=new int[x];
+        int[] sum=new int[x];
+        int summ=0;
+        for(int i=0;i<x;i++){
+            arr[i]=input.nextInt();
+        }
+        for(int i=0;i<x;i++){
+            summ=summ+arr[i];
+            sum[i]=summ;
+        }
+//        System.out.println(Arrays.toString(sum));
+        System.out.println(summ);
     }
 }
