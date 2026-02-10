8f6161addec1248ec94cb9cbd73f00af0a877f2a
Manasa <pillymanasa@gmail.com>
2026-02-10 22:08:54 +0530

    Daily progress

---
+++ src/Manasa/com/RichestCustomerWealth.java
@@ -0,0 +1,18 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int customer=input.nextInt();
+        int seller=input.nextInt();
+        int[][] sale=new int[customer][seller];
+        for(int i=0;i<customer;i++){
+            for(int j=0;j<seller;j++){
+
+            }
+        }
+
+
+    }
+}
