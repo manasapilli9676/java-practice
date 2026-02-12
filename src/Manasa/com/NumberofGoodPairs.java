01bb84a04aadd426eaf079a9e879ccd2ad69c405
Manasa <pillymanasa@gmail.com>
2026-02-12 21:48:58 +0530

    Daily progress

---
+++ src/Manasa/com/NumberofGoodPairs.java
@@ -0,0 +1,9 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class NumberofGoodPairs {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        
+    }
+}
