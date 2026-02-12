200edc8de7f202ec7cf703d3fbb0f35127223638
Manasa <pillymanasa@gmail.com>
2026-02-12 18:19:17 +0530

    Daily progress

--- src/Manasa/com/KidsWiththeGreatestNumberofCandies.java
+++ src/Manasa/com/KidsWiththeGreatestNumberofCandies.java
@@ -5,6 +5,18 @@ public class KidsWiththeGreatestNumberofCandies {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         int x=input.nextInt();
-        int[]
+        int[] candies=new int[x];
+        int extracandies=input.nextInt();
+        for(int i=0;i<x;i++){
+            candies[i]=input.nextInt();
+        }
+        int greater=candies[0];
+        int i=0;
+        while(x>candies.length-1){
+            if(candies[i]+extracandies>=greater){
+                System.out.println(Arrays.toString(candies));
+            }
+
+        }
     }
 }
