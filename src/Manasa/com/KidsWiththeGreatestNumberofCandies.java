0d9547f934fa5be16474a25c636b2a28e7399792
Manasa <pillymanasa@gmail.com>
2026-02-12 17:35:12 +0530

    Daily progress

---
+++ src/Manasa/com/KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,10 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class KidsWiththeGreatestNumberofCandies {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        int[]
+    }
+}
