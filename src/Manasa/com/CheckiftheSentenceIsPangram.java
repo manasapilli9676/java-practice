8eb903fa433c8d7cfeb6c120f140606b08d1b7d2
Manasa <pillymanasa@gmail.com>
2026-02-22 22:05:22 +0530

    Daily progress

--- src/Manasa/com/CheckiftheSentenceIsPangram.java
+++ src/Manasa/com/CheckiftheSentenceIsPangram.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class CheckiftheSentenceIsPangram {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String sentence=input.nextLine();
+        boolean[] alphabet=new boolean[26];
 
+        sentence=sentence.toLowerCase();
+        for(int i=0;i<sentence.length();i++){
+            if()
+        }
     }
 
 }
