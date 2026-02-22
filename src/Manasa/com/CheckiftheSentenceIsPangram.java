93b03d629071a9bcc5e5524634bc0cf95b748563
Manasa <pillymanasa@gmail.com>
2026-02-22 17:27:46 +0530

    Daily progress

---
+++ src/Manasa/com/CheckiftheSentenceIsPangram.java
@@ -0,0 +1,9 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class CheckiftheSentenceIsPangram {
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/Manasa/com/CreateTargetArrayintheGivenorder.java
+++ src/Manasa/com/CreateTargetArrayintheGivenorder.java
@@ -1,8 +1,42 @@
 package Manasa.com;
 import java.util.Scanner;
-import java.util.Arrays;
+
 public class CreateTargetArrayintheGivenorder {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        int[] index = new int[n];
+        int[] target = new int[n];
+
+        // Read nums array
+        for (int i = 0; i < n; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        // Read index array
+        for (int i = 0; i < n; i++) {
+            index[i] = input.nextInt();
+        }
+
+        // Create target array
+        for (int i = 0; i < n; i++) {
+
+            // Shift elements to the right
+            for (int j = n - 1; j > index[i]; j--) {
+                target[j] = target[j - 1];
+            }
+
+            // Insert element at correct position
+            target[index[i]] = nums[i];
+        }
+
+        // Print target array
+        for (int i = 0; i < n; i++) {
+            System.out.print(target[i] + " ");
+        }
     }
 }
