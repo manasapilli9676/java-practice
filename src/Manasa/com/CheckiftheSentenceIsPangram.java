82be039dffcff8890185e64173cf3a51fcb095a1
Manasa <pillymanasa@gmail.com>
2026-02-23 22:15:52 +0530

    Daily progress

--- src/Manasa/com/CheckiftheSentenceIsPangram.java
+++ src/Manasa/com/CheckiftheSentenceIsPangram.java
@@ -3,14 +3,34 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class CheckiftheSentenceIsPangram {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        String sentence=input.nextLine();
-        boolean[] alphabet=new boolean[26];
+        Scanner input = new Scanner(System.in);
+        String sentence = input.nextLine();
+        boolean[] alphabet = new boolean[26];
+        sentence = sentence.toLowerCase();
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                alphabet[ch - 'a'] = true;
 
-        sentence=sentence.toLowerCase();
-        for(int i=0;i<sentence.length();i++){
-            if()
+            }
+        }
+        boolean ispangram = true;
+        for (int i = 0; i < 26; i++) {
+            if(!alphabet[i]){
+            ispangram = false;
+            break;
         }
     }
+    if(ispangram)
 
+    {
+        System.out.println("The sentence is a Pangram.");
+    } else
+
+    {
+        System.out.println("The sentence is NOT a Pangram.");
+    }
+
+        input.close();
+}
 }
