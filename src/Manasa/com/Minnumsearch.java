4a40610507f815bd3b756e55ea685dfea50eb52d
Manasa <pillymanasa@gmail.com>
2026-02-28 12:45:05 +0530

    Daily progress

--- src/Manasa/com/Minnumsearch.java
+++ src/Manasa/com/Minnumsearch.java
@@ -5,22 +5,26 @@ import java.util.Arrays;
 public class Minnumsearch {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-//
+        System.out.println("enter the size of arr");
         int x=input.nextInt();
+
         int[] arr=new int[x];
+        System.out.println("enetr the input values");
                 for(int i=0;i<arr.length;i++){
                     arr[i]=input.nextInt();
                 }
 
+                int ans=search(arr);
+        System.out.println(ans);
     }
-    static int search(int[] arr,int max){
-        max=arr[0];
+    static int search(int[] arr){
+        int max=arr[0];
         if(arr.length==0){
             return -1;
         }
         for(int i=0;i<arr.length;i++){
-            if(max>arr[i]){
-                arr[i]=max;
+            if(max<arr[i]){
+               max=arr[i];
             }
             return i;
         }
