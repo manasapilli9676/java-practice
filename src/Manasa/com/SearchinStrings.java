a6e7823e35aeae34fef3a72e09087a17b2f2538e
Manasa <pillymanasa@gmail.com>
2026-02-28 10:30:07 +0530

    Daily progress

--- src/Manasa/com/SearchinStrings.java
+++ src/Manasa/com/SearchinStrings.java
@@ -1,4 +1,23 @@
 package Manasa.com;
-
+import java.util.Scanner;
 public class SearchinStrings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name="ram";
+        char element = 'm';
+        boolean ans =search(name,element);
+    }
+    static boolean search(String name,char element){
+        if(name.length()==0){
+            return false;
+        }
+        for(int i=0;i<name.length();i++){
+            if(element==name.charAt(i)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
 }
