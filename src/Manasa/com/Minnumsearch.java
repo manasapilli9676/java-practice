bcfa16892f539503e1f497cfa63b59f7bb6a8bb6
Manasa <pillymanasa@gmail.com>
2026-02-28 12:05:09 +0530

    Daily progress

---
+++ src/Manasa/com/Minnumsearch.java
@@ -0,0 +1,30 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Minnumsearch {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+//
+        int x=input.nextInt();
+        int[] arr=new int[x];
+                for(int i=0;i<arr.length;i++){
+                    arr[i]=input.nextInt();
+                }
+
+    }
+    static int search(int[] arr,int max){
+        max=arr[0];
+        if(arr.length==0){
+            return -1;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(max>arr[i]){
+                arr[i]=max;
+            }
+            return i;
+        }
+        return -1;
+    }
+//    return -1;
+}
