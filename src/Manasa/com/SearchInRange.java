d5a48159d05d38ef825f04422bff5c9005d807e0
Manasa <pillymanasa@gmail.com>
2026-02-28 11:40:32 +0530

    Daily progress

--- src/Manasa/com/SearchInRange.java
+++ src/Manasa/com/SearchInRange.java
@@ -5,10 +5,27 @@ public class SearchInRange {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         int x=input.nextInt();
+        int element=input.nextInt();
+        int start=input.nextInt();
+        int end=input.nextInt();
     int[] arr=new int[x];
+
     for(int i=0;i<arr.length;i++){
         arr[i]=input.nextInt();
     }
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
+
+    }
+    static int searchinrange(int[] arr,int element,int start,int end){
+        if(arr.length==0){
+            return -1;
+        }
+        for(int i=start;i<end;i++){
+            if(element==arr[i]){
+                return i;
+            }
+
+        }
+        return -1;
     }
 }
