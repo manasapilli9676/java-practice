78f512768f4a27a93d8e7705d28dfaebbe2a7dab
Manasa <pillymanasa@gmail.com>
2026-03-04 21:32:28 +0530

    Daily progress

---
+++ src/Manasa/com/Binarysearch.java
@@ -0,0 +1,17 @@
+package Manasa.com;
+import java.util.Scanner;
+/*
+* algorithm
+* 1.f*/
+import java.util.Arrays;
+public class Binarysearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         int x=input.nextInt();
+         int[] arr=new int[x];
+         for(int i=0;i<arr.length;i++){
+             arr[i]=input.nextInt();
+         }
+
+    }
+}
