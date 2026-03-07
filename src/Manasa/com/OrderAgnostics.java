0ff9691a0e1870f99666c587a484a54f566eb721
Manasa <pillymanasa@gmail.com>
2026-03-07 16:04:39 +0530

    Daily progress

--- src/Manasa/com/OrderAgnostics.java
+++ src/Manasa/com/OrderAgnostics.java
@@ -23,27 +23,34 @@ public class OrderAgnostics {
         int start = 0;
         int end = arr.length - 1;
 //        if(arr[start]>arr[end]) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+//        while (start <= end) {
+//            int mid = start + (end - start) / 2;
             if (arr[start] < arr[end]) {
-                if (target > arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            } else {
                 while (start <= end) {
+                    int mid = start + (end - start) / 2;
+                    if (target > arr[mid]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
+            }
+             else {
+                while (start <= end) {
+                    int mid = start + (end - start) / 2;
                     if (target > arr[mid]) {
                         end = mid + 1;
                     } else {
                         start = mid - 1;
                     }
-                    return mid;
+                    if(arr[mid]==target) {
+                        return mid;
+                    }
                 }
 
             }
 
-        }
+//        }
         return -1;
     }
 }
