076c155e571f3ac0539f6bae9d850cada7a8c1ab
Manasa <pillymanasa@gmail.com>
2026-03-07 15:22:50 +0530

    Daily progress

---
+++ src/Manasa/com/OrderAgnostics.java
@@ -0,0 +1,13 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class OrderAgnostics {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+    }
+    static int orderagnostic(int[] arr,int target){
+        int start=0;
+        int end=
+    }
+}
