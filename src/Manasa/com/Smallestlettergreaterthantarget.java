7cefc3cff6e9eadff22cb040ac6ba62e29ce2713
Manasa <pillymanasa@gmail.com>
2026-03-10 18:42:26 +0530

    Daily progress

---
+++ src/Manasa/com/Smallestlettergreaterthantarget.java
@@ -0,0 +1,11 @@
+package Manasa.com;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Smallestlettergreaterthantarget  {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+
+    }
+    static int target(int)
+}
