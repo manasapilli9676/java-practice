a9f556c75f17423b542896c8ad061753948a49ba
Manasa <pillymanasa@gmail.com>
2026-03-10 21:06:05 +0530

    Daily progress

--- src/Manasa/com/Smallestlettergreaterthantarget.java
+++ src/Manasa/com/Smallestlettergreaterthantarget.java
@@ -6,14 +6,28 @@ public class Smallestlettergreaterthantarget  {
         Scanner input=new Scanner(System.in);
         System.out.println("enter the size of arr");
         int x=input.nextInt();
-        int[] arr=new int[x];
+       char[] arr=new char[x];
         System.out.println("give the inputs");
         for(int i=0;i<arr.length;i++){
-            arr[i]=input.nextInt();
+            arr[i]=input.next().charAt(0);
         }
         System.out.println("enter the target value");
+        char target=input.next().charAt(0);
+        char result=target(arr,target);
+        System.out.println(result);
     }
-    static int target(int[] arr,int target){
-
+    static char target(char[] arr,char target){
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return arr[low%arr.length];
     }
 }
