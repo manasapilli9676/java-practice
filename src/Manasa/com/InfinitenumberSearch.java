ac6d08ac1cf5a74fde76542fdd69d4d6101eaf8a
Manasa <pillymanasa@gmail.com>
2026-03-14 11:53:07 +0530

    Daily progress

--- src/Manasa/com/InfinitenumberSearch.java
+++ src/Manasa/com/InfinitenumberSearch.java
@@ -4,11 +4,28 @@ import java.util.Arrays;
 public class InfinitenumberSearch{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        int[] arr={2,3,4,5,6,7,8,9,45,67,78,98,245,6758,7988};
+        int target=78;
+        int anss=targeeet(arr,target);
+        System.out.println(anss);
 
     }
-    static int binarysearch(int[] arr,int target){
+    static int targeeet(int[] arr,int target){
         int start=0;
-        int end=arr.length-1;
+        int end=1;
+        while(target>arr[end]){
+            int newstart=end+1;
+            //double the box size
+            //end=previous end+(sizeofbox)*2;
+            end=end+(end-start+1)*2;
+            start=newstart;
+        }
+        return binarysearch(arr,target,start,end);
+
+    }
+    static int binarysearch(int[] arr,int target,int start,int end){
+//        int start=0;
+//        int end=arr.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
             if(target<arr[mid]){
@@ -21,5 +38,6 @@ public class InfinitenumberSearch{
                 return mid;
             }
         }
+        return -1;
     }
 }
