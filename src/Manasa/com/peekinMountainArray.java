ff9da9d73ee2382288d76888f3cb88c46608958c
Manasa <pillymanasa@gmail.com>
2026-03-15 21:24:03 +0530

    Daily progress

--- src/Manasa/com/peekinMountainArray.java
+++ src/Manasa/com/peekinMountainArray.java
@@ -1,4 +1,18 @@
 package Manasa.com;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class peekinMountainArray {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+           int x=input.nextInt();
+           //for binary search array must be sorted
+        //no dublicate array
+
+    }
+    static int sorted(int[] arr,int target){
+
+    }
+    static int binarysearch(int[] arr,int target,int start,int eand){
+
+    }
 }
