890e8dfd7eeca7d4326c2a52b8e63377f43af86f
Manasa <pillymanasa@gmail.com>
2026-03-15 22:46:29 +0530

    Daily progress

--- src/Manasa/com/peekinMountainArray.java
+++ src/Manasa/com/peekinMountainArray.java
@@ -7,12 +7,47 @@ public class peekinMountainArray {
            int x=input.nextInt();
            //for binary search array must be sorted
         //no dublicate array
+        int[] arr=new int[x];
+        for(int i =0;i<arr.length;i++){
+
+        }
 
     }
+    //in the end start and end point to the same value
     static int sorted(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
 
+        while(start<end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            else{
+                //you are in tje decesing part of array
+                end=mid;
+            }
+        }
+        return start;
+        //where the start==end at the end of the array search so
     }
-    static int binarysearch(int[] arr,int target,int start,int eand){
-
-    }
+//    static int binarysearch(int[] arr,int target,int newstart,int newend){
+//        int start=0;
+//        int end=arr.length-1;
+////        int newstart=0;
+////        int newend=0;
+//        while(start<end) {
+//            int mid = start + (end - start) / 2;
+//            if(arr[mid]<arr[mid+1]){
+//                newstart=mid+1;
+//            }
+//            else{
+//                newend=mid;
+//            }
+//        }
+//        while(newstart<newend){
+//            if(arr[])
+//        }
+//
+//    }
 }
