71a5af3d97d664764dc543d7bd0aea5b9e0dc191
Manasa <pillymanasa@gmail.com>
2026-03-18 12:18:52 +0530

    Daily progress

--- src/Manasa/com/Sortedrotatedarray.java
+++ src/Manasa/com/Sortedrotatedarray.java
@@ -2,6 +2,21 @@ package Manasa.com;
 import java.util.Arrays;
 public class Sortedrotatedarray {
     public static void main(String[] args) {
-
+        int[] arr={3,4,5,6,7,1,2};
+        int ans=search(arr);
+    }
+    static int search(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[end]){
+                start=mid+1;
+            }
+            else{
+                end=start;
+            }
+        }
+        return start;
     }
 }
