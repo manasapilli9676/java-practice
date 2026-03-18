6d05f0dcac954f4686f77e91cdbd20e2515149f8
Manasa <pillymanasa@gmail.com>
2026-03-18 13:13:57 +0530

    Daily progress

--- src/Manasa/com/Sortedrotatedarray.java
+++ src/Manasa/com/Sortedrotatedarray.java
@@ -1,22 +1,59 @@
+//package Manasa.com;
+//import java.util.Arrays;
+//public class Sortedrotatedarray {
+//    public static void main(String[] args) {
+//        int[] arr={3,4,5,6,7,1,2};
+//        int ans=search(arr);
+//        System.out.println(ans);
+//        //it is not for duplicate arr
+//
+//    }
+//    static int search(int[] arr){
+//        int start=0;
+//        int end=arr.length-1;
+//        while(start<=end){
+//            int mid=start+(end-start)/2;
+//            //four cases we can see here
+//            if(mid<end && arr[mid]>arr[mid+1]){
+//                return mid;
+//            }
+//            if(mid>start&&arr[mid]<arr[mid-1]){
+//                return mid-1;
+//            }
+//            if(arr[mid]<=arr[start]){
+//                end=mid-1;
+//            }else{
+//                start=mid+1;
+//            }
+////            if(arr[mid]>=arr[end]){
+////                start=mid;
+////            }
+////            else{
+////                end=mid-1;
+////            }
+//        }
+//        return -1;
+//    }
+//}
 package Manasa.com;
 import java.util.Arrays;
 public class Sortedrotatedarray {
     public static void main(String[] args) {
         int[] arr={3,4,5,6,7,1,2};
         int ans=search(arr);
+        System.out.println(ans);
     }
     static int search(int[] arr){
         int start=0;
         int end=arr.length-1;
         while(start<end){
-            int mid=start+(end-start)/2;
+            int mid= start+(end-start)/2;
             if(arr[mid]>arr[end]){
                 start=mid+1;
-            }
-            else{
+            }else{
                 end=mid;
             }
         }
-        return start;
+        return (start-1+arr.length)%arr.length;
     }
 }
