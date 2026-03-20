6d64fbc8366297c0a647621dc479b2326bb68954
Manasa <pillymanasa@gmail.com>
2026-03-20 13:11:07 -0400

    Daily progress

--- src/Manasa/com/Sortedrotatedarray.java
+++ src/Manasa/com/Sortedrotatedarray.java
@@ -2,7 +2,8 @@ package Manasa.com;
 import java.util.Arrays;
 public class Sortedrotatedarray {
     public static void main(String[] args) {
-
+        int[] arr={4,5,6,7,0,1,2};
+        System.out.println(pivote(arr));
     }
     //here for finding the target element we are using 5 approaches so
     //first one is find pivote elemet using it we can solve by two cases
@@ -11,12 +12,19 @@ public class Sortedrotatedarray {
         int end=arr.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
+            //here mid
             if(mid<end&&arr[mid]>arr[mid+1]){
                 return mid;
             }
-            if(arr[mid]<arr[mid-1]){
+            if(mid>start&&arr[mid]<arr[mid-1]){
                 return mid-1;
             }
+            if(arr[mid]<=arr[start]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+
         }
         return -1;
     }
