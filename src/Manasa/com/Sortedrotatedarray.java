b597983f03f58ea946897c765ccdb4d16f326311
Manasa <pillymanasa@gmail.com>
2026-03-20 12:51:29 -0400

    Daily progress

--- src/Manasa/com/Sortedrotatedarray.java
+++ src/Manasa/com/Sortedrotatedarray.java
@@ -11,11 +11,16 @@ public class Sortedrotatedarray {
         int end=arr.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(arr[mid]>arr[start]){
-
+            if(mid<end&&arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(arr[mid]<arr[mid-1]){
+                return mid-1;
             }
         }
+        return -1;
     }
+
 }
 
 //package Manasa.com;
