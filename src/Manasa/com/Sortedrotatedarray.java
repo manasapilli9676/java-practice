aed0619234a15944b87238c3ab0e908242d14dcd
Manasa <pillymanasa@gmail.com>
2026-03-20 14:02:10 -0400

    Daily progress

--- src/Manasa/com/Sortedrotatedarray.java
+++ src/Manasa/com/Sortedrotatedarray.java
@@ -4,9 +4,33 @@ public class Sortedrotatedarray {
     public static void main(String[] args) {
         int[] arr={4,5,6,7,0,1,2};
         System.out.println(pivote(arr));
+        int target=
     }
     //here for finding the target element we are using 5 approaches so
     //first one is find pivote elemet using it we can solve by two cases
+    static int search(int[] arr,int target,int start,int end){
+        int pivot=pivote(arr);
+        //if you didnot find sthe pivote
+        if(pivot==-1){
+            //just do normal binary search
+            return binarysearch(arr,target,start,end);
+        }
+
+    }
+    static int binarysearch(int[] arr,int target,int start,int end){
+
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target){
+                start=mid+1;
+            }else if(arr[mid]<target){
+                end=mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
     static int pivote(int[] arr){
         int start=0;
         int end=arr.length-1;
