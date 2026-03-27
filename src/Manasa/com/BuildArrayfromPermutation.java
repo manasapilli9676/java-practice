8f9559d044e4acdb6261eb31d0a5ef1dddf98c56
Manasa <pillymanasa@gmail.com>
2026-03-26 23:00:17 -0400

    Daily progress

--- src/Manasa/com/BuildArrayfromPermutation.java
+++ src/Manasa/com/BuildArrayfromPermutation.java
@@ -31,24 +31,18 @@ public class BuildArrayfromPermutation {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Size of array:");
         int x = input.nextInt();
-
         int[] arr = new int[x];
         int[] ans = new int[x];
-
         System.out.println("Enter permutation values:");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = input.nextInt();
         }
-
         // Build Array from Permutation logic
         for (int i = 0; i < arr.length; i++) {
             ans[i] = arr[arr[i]];
         }
-
-
         System.out.println("Original array: " + Arrays.toString(arr));
         System.out.println("Built array: " + Arrays.toString(ans));
     }
