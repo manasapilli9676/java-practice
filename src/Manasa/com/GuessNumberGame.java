3152cbefcbf5eae2b9b205d3932b77a559c6a3fe
Manasa <pillymanasa@gmail.com>
2026-03-28 23:06:35 -0400

    Daily progress

--- src/Manasa/com/GuessNumberGame.java
+++ src/Manasa/com/GuessNumberGame.java
@@ -1,4 +1,30 @@
 package Manasa.com;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int numberToGuess = rand.nextInt(100) + 1; // 1 to 100
+        int userGuess = 0;
+        int attempts = 0;
+
+        System.out.println("Guess a number between 1 and 100:");
+
+        while (userGuess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            userGuess = sc.nextInt();
+            attempts++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("🎉 Correct! You guessed it in " + attempts + " attempts.");
+            }
+        }
+    }
 }
