92eebb85fbfdf947b28161875f01946d020de6f4
Manasa <pillymanasa@gmail.com>
2026-04-06 22:48:44 +0530

    Daily progress

--- src/Manasa/com/PalindromeMethodjava.java
+++ src/Manasa/com/PalindromeMethodjava.java
@@ -1,6 +1,7 @@
 package Manasa.com;
 import java.util.Scanner;
 public class PalindromeMethodjava {
+
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         int num=input.nextInt();
