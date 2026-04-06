00494fdf4aa14129b01057e9be2a89fd55dcec13
Manasa <pillymanasa@gmail.com>
2026-04-06 22:49:37 +0530

    Daily progress

--- src/Manasa/com/ArrayFunctions.java
+++ src/Manasa/com/ArrayFunctions.java
@@ -2,7 +2,6 @@ package Manasa.com;
 import java.util.Scanner;
 import java.util.Arrays;
 public class ArrayFunctions {
-
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         int[] nums={2,3,4,5,6,5};
